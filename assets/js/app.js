7d44f23db968d331b777d59142d5e793dee53259
Justin S. Baiko <baiko@utexas.edu>
2016-09-08 12:57:06 -0500

    add button view

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,30 @@
 var APIkey = "dc6zaTOxFJmzC";
 var url = "http://api.giphy.com/v1/gifs/search?q=";
 var search;
+	var movies = ['The Matrix', 'The Notebook', 'Mr. Nobody', 'The Lion King'];
 
 $(document).ready (function() {
   $('.title').html("VEGETABLES");
+  renderButtons();
+
+  function renderButtons(){
+
+		// Deletes the movies prior to adding new movies (this is necessary otherwise you will have repeat buttons)
+		$('#buttonMania').empty();
+
+		// Loops through the array of movies
+		for (var i = 0; i < movies.length; i++){
+
+			// Then dynamicaly generates buttons for each movie in the array
+
+			// Note the jQUery syntax here...
+		    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
+		    a.addClass('movie'); // Added a class
+		    a.attr('data-name', movies[i]); // Added a data-attribute
+		    a.text(movies[i]); // Provided the initial button text
+		    $('#buttonMania').append(a); // Added the button to the HTML
+		}
+	}
 
   console.log("search value is: " + search);
 
@@ -35,6 +56,10 @@ $(document).ready (function() {
               $('#gifsAppearHere').append(gifDiv);
           }
       });
+
+      movies.push(search);
+      renderButtons();
+      
   });
 
 
